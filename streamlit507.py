4c2a06113037d341b8cb730884e1b25b57f6aea7
nathalialadino <91520264+nathalialadino@users.noreply.github.com>
2021-12-15 19:29:27 -0500

    Create streamlit507.py

---
+++ streamlit507.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 15 19:27:59 2021
+
+@author: natha
+"""
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import time
+
+@st.cache
+def load_hospitals():
+    df_hospital_2 = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_hospital_2.csv')
+    return df_hospital_2
+@st.cache
+def load_inatpatient():
+    df_inpatient_2 = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_inpatient_2.csv')
+    return df_inpatient_2
+
+@st.cache
+def load_outpatient():
+    df_outpatient_2 = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_outpatient_2.csv')
+    return df_outpatient_2
+
+st.title('Hospital data analysis')
+
+streamlit run hha507final.py
