fb46b79911086d4b83263e2a1d16a6b86f311fd4
nathalialadino <91520264+nathalialadino@users.noreply.github.com>
2021-12-15 23:11:21 -0500

    Update streamlit507.py

--- streamlit507.py
+++ streamlit507.py
@@ -4,6 +4,7 @@ Created on Wed Dec 15 19:27:59 2021
 
 @author: natha
 """
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -42,4 +43,5 @@ st.dataframe(nyinpatient)
 
 st.header('New York Outpatient Data')
 st.dataframe(nyoutpatient)
+
 
