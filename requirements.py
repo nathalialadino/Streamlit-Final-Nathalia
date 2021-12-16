5abe0b7bd73f9a9a8e642c2cafbe35cddf98f1fc
nathalialadino <91520264+nathalialadino@users.noreply.github.com>
2021-12-15 19:53:28 -0500

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 15 19:43:52 2021
+
+@author: natha
+"""
+pip install pipreqs
+scipy
+plotly
+pandas 
+numpy
+matplotlib
+seaborn
+sklearn
+streamlit
+
